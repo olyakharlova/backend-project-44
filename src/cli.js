fd7ab9226ed3d7b309fb93300421f89c89862d20
olya kharlova <pepedrizzle@gmail.com>
2022-11-13 15:22:25 +0300

    cli.js updated due to the linter check

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
-export var userName = readlineSync.question('May I have your name, please?');
+export const userName = readlineSync.question('May I have your name, please?');
 console.log('Hi,', userName, '!');
