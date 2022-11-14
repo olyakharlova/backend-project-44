404bbb35f8f1e5bc7d638703eea9b65638ebe8dc
olya kharlova <pepedrizzle@gmail.com>
2022-11-14 10:09:20 +0300

    games directory added

---
+++ src/games/game-even.js
@@ -0,0 +1,18 @@
+import getRandomNumber from '../getrandomNumber.js';
+import playGame from '../index.js';
+
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEvenNumber = (num) => num % 2 === 0;
+
+const getQuestionAndAnswer = () => {
+    const question = getRandomNumber();
+    const answer = isEvenNumber(question) ? 'yes' : 'no';
+    return [question, answer];
+};
+
+const initGameEven = () => {
+    playGame(rule, getQuestionAndAnswer);
+};
+
+export default initGameEven;
