b9852cb7ad91fa410307624674a6d700b304a2c1
olya kharlova <pepedrizzle@gmail.com>
2022-12-28 12:30:08 +0300

    getRandomNumber.js fixed to meet linter requirements

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,7 +1,8 @@
 const maxNumRand = 100;
 const minNumRand = 0;
 
-const getRandomNumber = (minNum = minNumRand,
-  maxNum = maxNumRand) => Math.round(Math.random() * maxNum) + minNum;
+const getRandomNumber = (minNum = minNumRand, maxNum = maxNumRand) => (
+  Math.round(Math.random() * maxNum) + minNum
+);
 
 export default getRandomNumber;
